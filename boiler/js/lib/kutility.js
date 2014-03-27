7220a13e32b22b132dd9707bffaa41f9f87c3a57
Kevin Roark <ker2143@columbia.edu>
2014-03-27 18:29:08 -0400

    bump kutility

--- boiler/js/lib/kutility.js
+++ boiler/js/lib/kutility.js
@@ -43,9 +43,9 @@ Kutility.prototype.randColor = function() {
     var num = entry % 256;
 
     if(entry < 256 * 1)
-      return text + "0,255," + num + ")";
+    	return text + "0,255," + num + ")";
     else if(entry < 256 * 2)
-      return text + "0," + (255 - num) + ",255)";
+    	return text + "0," + (255 - num) + ",255)";
     else if(entry < 256 * 3)
       return text + num + ",0,255)";
     else if(entry < 256 * 4)
@@ -90,4 +90,305 @@ Kutility.prototype.addShadow = function(el, size) {
   el.css('-webkit-box-shadow', shadow);
   el.css('-moz-box-shadow', shadow);
   el.css('box-shadow', shadow);
+}
+
+/**
+ * Add transform to element with all the lame browser prefixes.
+ *
+ * @api public
+ */
+Kutility.prototype.addTransform = function(el, transform) {
+  var curTransform = this.getTransform(el);
+  curTransform = curTransform.replace('none', '');
+  var newTransform = curTransform + transform;
+  this.setTransform(el, newTransform);
+}
+
+/**
+ * Set transform of element with all the lame browser prefixes.
+ *
+ * @api public
+ */
+Kutility.prototype.setTransform = function(el, transform) {
+  el.css('-webkit-transform', transform);
+  el.css('-moz-transform', transform);
+  el.css('-ms-transform', transform);
+  el.css('-o-transform', transform);
+  el.css('transform', transform);
+}
+
+/**
+ * Check an elements tansform.
+ *
+ * @api public
+ */
+Kutility.prototype.getTransform = function(el) {
+  var possible = ['transform', '-webkit-transform', '-moz-transform', '-ms-transform', '-o-transform'];
+
+  for (var i = 0; i < possible.length; i++) {
+    var f = el.css(possible[i]);
+    if (f == 'none' && i + 1 < possible.length) {
+      var pf = el.css(possible[i + 1]);
+      if (pf)
+        continue;
+    }
+    return f;
+  }
+}
+
+/**
+ * Remove all transforms from element.
+ *
+ * @api public
+ */
+Kutility.prototype.clearTransforms = function(el) {
+  el.css('-webkit-transform', '');
+  el.css('-moz-transform', '');
+  el.css('-ms-transform', '');
+  el.css('-o-transform', '');
+  el.css('transform', '');
+}
+
+/**
+ * Rotate an element by x degrees.
+ *
+ * @api public
+ */
+Kutility.prototype.rotate = function(el, x) {
+  var ct = this.getTransform(el);
+  ct = ct.replace(/matrix\(.*\)/, '').replace('none', '');
+
+  var t = ' rotate(' + x + 'deg)';
+  this.setTransform(el, ct  + t);
+}
+
+/**
+ * Scale an element by x (no units);
+ *
+ * @api public
+ */
+Kutility.prototype.scale = function(el, x) {
+  var ct = this.getTransform(el);
+  ct = ct.replace(/scale\(.*\)/, '').replace('none', '');
+
+  var t = ' scale(' + x + ')';
+  this.setTransform(el, ct + t);
+}
+
+/**
+ * Translate an element by x, y (include your own units);
+ *
+ * @api public
+ */
+Kutility.prototype.translate = function(el, x, y) {
+  var ct = this.getTransform(el);
+  ct = ct.replace(/translate\(.*\)/, '').replace('none', '');
+
+  var t = ' translate(' + x + ', '  + y + ')';
+  this.setTransform(el, ct + t);
+}
+
+/**
+ * Skew an element by x, y degrees;
+ *
+ * @api public
+ */
+Kutility.prototype.skew = function(el, x, y) {
+  var ct = this.getTransform(el);
+  ct = ct.replace(/skew\(.*\)/, '').replace('none', '');
+
+  var xd = x + 'deg';
+  var yd = y + 'deg';
+  var t = ' skew(' + xd + ', ' + yd + ')';
+  this.setTransform(el, ct + t);
+}
+
+/**
+ * Warp an element by a random amount by rotating and skewing it.
+ *
+ * @api public
+ */
+Kutility.prototype.warp = function(el) {
+  var r = Math.floor(Math.random() * 360);
+  var x = Math.floor(Math.random() * 360);
+  var y = Math.floor(Math.random() * 360);
+
+  this.rotate(el, r);
+  this.skew(el, x, y);
+}
+
+/**
+ * Add filter to element with all the lame browser prefixes.
+ *
+ * @api public
+ */
+Kutility.prototype.addFilter = function(el, filter) {
+  var curFilter = this.getFilter(el);
+  curFilter = curFilter.replace('none', '');
+  var newFilter = curFilter + ' ' + filter;
+  this.setFilter(el, newFilter);
+}
+
+/**
+ * Set filter to element with all lame prefixes.
+ *
+ * @api public
+ */
+Kutility.prototype.setFilter = function(el, filter) {
+  el.css('-webkit-filter', filter);
+  el.css('-moz-filter', filter);
+  el.css('-ms-filter', filter);
+  el.css('-o-filter', filter);
+  el.css('filter', filter);
+}
+
+/**
+ * Check an elements filter.
+ *
+ * @api public
+ */
+Kutility.prototype.getFilter = function(el) {
+  var possible = ['filter', '-webkit-filter', '-moz-filter', '-ms-filter', '-o-filter'];
+
+  for (var i = 0; i < possible.length; i++) {
+    var f = el.css(possible[i]);
+    if (f == 'none' && i + 1 < possible.length) {
+      var pf = el.css(possible[i + 1]);
+      if (pf)
+        continue;
+    }
+    return f;
+  }
+}
+
+/**
+ * Remove all filters from element.
+ *
+ * @api public
+ */
+Kutility.prototype.clearFilters = function(el) {
+  el.css('-webkit-filter', '');
+  el.css('-moz-filter', '');
+  el.css('-ms-filter', '');
+  el.css('-o-filter', '');
+  el.css('filter', '');
+}
+
+/**
+
+/**
+ * Grayscale an element by x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.grayscale = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/grayscale\(.*\)/, '').replace('none', '');
+
+  var f = ' grayscale(' + x + '%)';
+  this.setFilter(el, cf  + f);
+}
+
+/**
+ * Sepia an element by x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.sepia = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/sepia\(.*\)/, '').replace('none', '');
+
+  var f = ' sepia(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Saturate an element by x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.saturate = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/saturate\(.*\)/, '').replace('none', '');
+
+  var f = ' saturate(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Invert an element by x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.invert = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/invert\(.*\)/, '').replace('none', '');
+
+  var f = ' invert(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Hue-rotate an element by x degrees.
+ *
+ * @api public
+ */
+Kutility.prototype.hutate = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/hue-rotate\(.*\)/, '').replace('none', '');
+
+  var f = ' hue-rotate(' + x + 'deg)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Set opacity of an element to x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.opace = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/opacity\(.*\)/, '').replace('none', '');
+
+  var f = ' opacity(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Set brightness of an element to x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.brightness = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/brightness\(.*\)/, '').replace('none', '');
+
+  var f = ' brightness(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Set contrast of an element to x percent.
+ *
+ * @api public
+ */
+Kutility.prototype.contrast = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/contrast\(.*\)/, '').replace('none', '');
+
+  var f = ' contrast(' + x + '%)';
+  this.setFilter(el, cf + f);
+}
+
+/**
+ * Blur an element by x pixels.
+ *
+ * @api public
+ */
+Kutility.prototype.blur = function(el, x) {
+  var cf = this.getFilter(el);
+  cf = cf.replace(/blur\(.*\)/, '').replace('none', '');
+
+  var f = ' blur(' + x + 'px)';
+  this.setFilter(el, cf + f);
 }
