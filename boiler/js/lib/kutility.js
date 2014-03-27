f49bbc4ed07b90223a21649315517cb177921f22
Kevin Roark <ker2143@columbia.edu>
2014-03-27 12:49:56 -0400

    new kutility

--- boiler/js/lib/kutility.js
+++ boiler/js/lib/kutility.js
@@ -11,7 +11,7 @@ function Kutility() {
  *
  * @api public
  */
- 
+
 Kutility.prototype.choice = function(arr) {
     var i = Math.floor(Math.random() * arr.length);
     return arr[i];
@@ -27,4 +27,67 @@ Kutility.prototype.randColor = function() {
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
     return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+/**
+ * Color wheel 1 -> 1536.
+ *
+ * Written by Henry Van Dusen, all attribution to the big boy.
+ * Slightly modified by Kev.
+ *
+ * @api public
+ */
+ Kutility.prototype.colorWheel = function(num) {
+    var text = "rgb(";
+    var entry = num % 1536;
+    var num = entry % 256;
+
+    if(entry < 256 * 1)
+      return text + "0,255," + num + ")";
+    else if(entry < 256 * 2)
+      return text + "0," + (255 - num) + ",255)";
+    else if(entry < 256 * 3)
+      return text + num + ",0,255)";
+    else if(entry < 256 * 4)
+      return text + "255,0," + (255 - num) + ")";
+    else if(entry < 256 * 5)
+      return text + "255," + num + ",0)";
+    else
+      return text + (255 - num) + ",255,0)";
+ }
+
+ /**
+  * Make an rbg() color string an rgba() color string
+  *
+  * @api public
+  */
+Kutility.prototype.alphize = function(color, alpha) {
+  color.replace('rgb', 'rgba');
+  color.replace(')', ', ' + alpha + ')');
+  return color;
+}
+
+/**
+ * Get an array of two random contrasting colors.
+ *
+ * @api public
+ */
+Kutility.prototype.contrasters = function() {
+  var num = Math.floor(Math.random() * 1536);
+  var fg = this.colorWheel(num);
+  var bg = this.colorWheel(num + 650);
+  return [fg, bg];
+}
+
+/**
+ * Add a random shadow to a jquery element
+ *
+ * @api public
+ */
+Kutility.prototype.addShadow = function(el, size) {
+  var s = size + 'px';
+  var shadow = '0px 0px ' + s + ' ' + s + ' ' + this.randColor();
+  el.css('-webkit-box-shadow', shadow);
+  el.css('-moz-box-shadow', shadow);
+  el.css('box-shadow', shadow);
 }
